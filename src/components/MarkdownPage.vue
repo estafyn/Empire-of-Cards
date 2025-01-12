e88bd3edbbbe7ddea82db03eedbcd3235140034f
James Rushford <jrushford@gitlab.com>
2025-01-12 12:33:47 +0100

    Fix structure of repo

--- src/components/MarkdownPage.vue
+++ src/components/MarkdownPage.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import { defineProps } from 'vue'
 import MarkdownIt from 'markdown-it'
 
 defineProps<{ content: string; title: string }>()
--- src/components/TableOfContents.vue
+++ src/components/TableOfContents.vue
@@ -1,6 +1,4 @@
 <script setup lang="ts">
-import { defineProps, defineEmits } from 'vue'
-
 type FileEntry = {
   name: string
   path: string
